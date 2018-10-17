426a2d221f38b2d0d7289691514370029c093fa8
Yessica-C <tryben4242@gmail.com>
2018-10-16 20:27:07 -0700

    continuing to work on arm test

--- TeamCode/src/main/java/teamcode/ArmTest.java
+++ TeamCode/src/main/java/teamcode/ArmTest.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import java.lang.reflect.Array;
 
 
-@TeleOp(name = "TestClass", group = "Linear Opmode") //making the code appear in the app
+@TeleOp(name = "ARM TEST", group = "Linear Opmode") //making the code appear in the app
 
 
 
@@ -17,6 +17,7 @@ public class ArmTest extends LinearOpMode { //make it work right
     private Servo shoulderJoint;
     private Servo elbowJoint;
     private Servo wristJoint;
+    private Servo intakeHand;
 
     @Override
     public void runOpMode() {
@@ -25,33 +26,89 @@ public class ArmTest extends LinearOpMode { //make it work right
         this.shoulderJoint = hardwareMap.get(Servo.class, "Shoulder");
         this.elbowJoint = hardwareMap.get(Servo.class, "Elbow");
         this.wristJoint = hardwareMap.get(Servo.class, "Wrist");
+        this.intakeHand = hardwareMap.get(Servo.class, "Hand");
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            String[] motorList = new String[3];
+            Servo[] motorList = new Servo[3];
 
             int selectedJoint  = 0;
 
-            motorList[0] = ("Base");
-            motorList[1] = ("Shoulder");
-            motorList[2] = ("Elbow");
-            motorList[3] = ("Wrist");
+            motorList[0] = (baseJoint);
+            motorList[1] = (shoulderJoint);
+            motorList[2] = (elbowJoint);
+            motorList[3] = (wristJoint);
 
             if(gamepad1.a){
 
-                if(selectedJoint == 3 ){
+                if(selectedJoint == 3){
 
-
+                    selectedJoint = 0;
+                    telemetry.addData("Motor in use", motorList[selectedJoint]);
+                    telemetry.update();
 
                 }
 
+                else if(selectedJoint != 3){
+
+                    selectedJoint++;
+                    telemetry.addData("Motor in use", motorList[selectedJoint]);
+                    telemetry.update();
+
+                }
             }
 
+            if (gamepad1.left_bumper){
 
+                motorList[selectedJoint].setPosition((motorList[selectedJoint].getPosition()) - 0.01);
 
+                sleep(50);
 
+                telemetry.addData("Base Pos", baseJoint.getPosition());
+                telemetry.addData("Shoulder Pos", shoulderJoint.getPosition());
+                telemetry.addData("Elbow Pos", elbowJoint.getPosition());
+                telemetry.addData("Wrist Pos", wristJoint.getPosition());
+
+                telemetry.update();
+
+            }
+
+            if (gamepad1.left_bumper){
+
+                motorList[selectedJoint].setPosition((motorList[selectedJoint].getPosition()) + 0.01);
+
+                sleep(50);
+
+                telemetry.addData("Base Pos", baseJoint.getPosition());
+                telemetry.addData("Shoulder Pos", shoulderJoint.getPosition());
+                telemetry.addData("Elbow Pos", elbowJoint.getPosition());
+                telemetry.addData("Wrist Pos", wristJoint.getPosition());
+
+                telemetry.update();
+
+            }
+
+            if(gamepad1.b){
+
+                intakeHand.setPosition(1.0);
+
+                sleep(150);
+
+                intakeHand.setPosition(0.5);
+
+            }
+
+            if(gamepad1.x){
+
+                intakeHand.setPosition(0);
+
+                sleep(150);
+
+                intakeHand.setPosition(0.5);
+
+            }
         }
     }
 }
