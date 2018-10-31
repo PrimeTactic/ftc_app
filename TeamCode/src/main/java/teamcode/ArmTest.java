228775a08c2268b497ab47086aeb90f6c6bbe671
Yessica-C <tryben4242@gmail.com>
2018-10-30 18:46:16 -0700

    Arm changes

--- TeamCode/src/main/java/teamcode/ArmTest.java
+++ TeamCode/src/main/java/teamcode/ArmTest.java
@@ -3,6 +3,7 @@ package teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import java.lang.reflect.Array;
 
@@ -13,17 +14,21 @@ import java.lang.reflect.Array;
 
 public class ArmTest extends LinearOpMode { //make it work right
 
-    private Servo baseJoint;
-    private Servo shoulderJoint;
+    private DcMotor baseJoint;
     private Servo elbowJoint;
     private Servo wristJoint;
     private Servo intakeHand;
 
+    public String currentDevice = "Base";
+
     @Override
     public void runOpMode() {
 
-        this.baseJoint= hardwareMap.get(Servo.class, "Base");
-        this.shoulderJoint = hardwareMap.get(Servo.class, "Shoulder");
+        this.baseJoint = hardwareMap.get(DcMotor.class, "Base");
+
+        baseJoint.setMode   (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        baseJoint.setMode    (DcMotor.RunMode.RUN_TO_POSITION);
+
         this.elbowJoint = hardwareMap.get(Servo.class, "Elbow");
         this.wristJoint = hardwareMap.get(Servo.class, "Wrist");
         this.intakeHand = hardwareMap.get(Servo.class, "Hand");
@@ -32,83 +37,167 @@ public class ArmTest extends LinearOpMode { //make it work right
 
         while (opModeIsActive()) {
 
-            Servo[] motorList = new Servo[3];
+            selectDevice();
 
-            int selectedJoint  = 0;
+            if(gamepad1.y){
 
-            motorList[0] = (baseJoint);
-            motorList[1] = (shoulderJoint);
-            motorList[2] = (elbowJoint);
-            motorList[3] = (wristJoint);
-
-            if(gamepad1.a){
-
-                if(selectedJoint == 3){
-
-                    selectedJoint = 0;
-                    telemetry.addData("Motor in use", motorList[selectedJoint]);
-                    telemetry.update();
-
-                }
-
-                else if(selectedJoint != 3){
-
-                    selectedJoint++;
-                    telemetry.addData("Motor in use", motorList[selectedJoint]);
-                    telemetry.update();
-
-                }
-            }
-
-            if (gamepad1.left_bumper){
-
-                motorList[selectedJoint].setPosition((motorList[selectedJoint].getPosition()) - 0.01);
-
-                sleep(50);
-
-                telemetry.addData("Base Pos", baseJoint.getPosition());
-                telemetry.addData("Shoulder Pos", shoulderJoint.getPosition());
-                telemetry.addData("Elbow Pos", elbowJoint.getPosition());
-                telemetry.addData("Wrist Pos", wristJoint.getPosition());
-
-                telemetry.update();
+                baseJoint.setTargetPosition(200);
 
             }
 
-            if (gamepad1.left_bumper){
+            while(currentDevice == "Hand"){//hand must be continuous
 
-                motorList[selectedJoint].setPosition((motorList[selectedJoint].getPosition()) + 0.01);
+                if(gamepad1.x){
 
-                sleep(50);
+                    this.intakeHand.setPosition(1.0);
+                    sleep(150);
+                    this.intakeHand.setPosition(0.5);
 
-                telemetry.addData("Base Pos", baseJoint.getPosition());
-                telemetry.addData("Shoulder Pos", shoulderJoint.getPosition());
-                telemetry.addData("Elbow Pos", elbowJoint.getPosition());
-                telemetry.addData("Wrist Pos", wristJoint.getPosition());
+                }
 
-                telemetry.update();
+                if(gamepad1.b){
+
+                    this.intakeHand.setPosition(0.0);
+                    sleep(150);
+                    this.intakeHand.setPosition(0.5);
+
+                }
+
+                selectDevice();
+            }
+
+
+            while(currentDevice == "Wrist"){//wrist must use encoders
+
+                if(gamepad1.x){
+
+                    this.wristJoint.setPosition(this.wristJoint.getPosition() - 0.01);
+
+                }
+
+                if(gamepad1.b){
+
+                    this.wristJoint.setPosition(this.wristJoint.getPosition() + 0.01);
+
+                }
+
+                selectDevice();
+            }
+
+
+            while(currentDevice == "Elbow"){//elbow must use encoders
+
+                if(gamepad1.x){
+
+                    this.elbowJoint.setPosition(this.elbowJoint.getPosition() - 0.01);
+
+                }
+
+                if(gamepad1.b){
+
+                    this.elbowJoint.setPosition(this.elbowJoint.getPosition() + 0.01);
+
+                }
+
+                selectDevice();
+            }
+
+            while(currentDevice == "Base"){//the base is a motor using encoders
+
+                if(gamepad1.x){
+
+                    baseJoint.setTargetPosition(baseJoint.getCurrentPosition() + 10);
+
+                }
+
+                if(gamepad1.b){
+
+                    baseJoint.setTargetPosition(baseJoint.getCurrentPosition() - 10);
+
+                }
+
+                runBaseToPos();
+                selectDevice();
+
+            }
+
+
+        }
+    }
+
+    public void selectDevice(){
+        if(gamepad1.dpad_up){
+
+            currentDevice = "Base";
+
+        }
+
+        if(gamepad1.dpad_down){
+
+            currentDevice = "Elbow";
+
+        }
+
+        if(gamepad1.dpad_left){
+
+            currentDevice = "Wrist";
+
+        }
+
+        if(gamepad1.dpad_right){
+
+            currentDevice = "Hand";
+
+
+        }
+
+        updateTelemetry();
+    }
+
+    public void updateTelemetry(){
+
+        telemetry.addData("Selected device", currentDevice);
+        telemetry.addData("Hand Speed", intakeHand.getPosition());
+        telemetry.addData("Wrist Pos", wristJoint.getPosition());
+        telemetry.addData("Elbow Pos", elbowJoint.getPosition());
+        telemetry.addData("Base Pos", baseJoint.getCurrentPosition());
+
+        telemetry.addData("Base Targer", baseJoint.getTargetPosition());
+
+        telemetry.update();
+
+    }
+
+    public void runBaseToPos(){
+
+        baseJoint.setPower(1);
+
+        while(!motorWithinTarget()) {
+
+        telemetry.update();
+
+            if(gamepad1.x){
+
+                baseJoint.setTargetPosition(baseJoint.getCurrentPosition() + 10);
 
             }
 
             if(gamepad1.b){
 
-                intakeHand.setPosition(1.0);
-
-                sleep(150);
-
-                intakeHand.setPosition(0.5);
+                baseJoint.setTargetPosition(baseJoint.getCurrentPosition() - 10);
 
             }
 
-            if(gamepad1.x){
-
-                intakeHand.setPosition(0);
-
-                sleep(150);
-
-                intakeHand.setPosition(0.5);
-
-            }
         }
+
+        baseJoint.setPower(0);
+    }
+
+    public boolean motorWithinTarget(){
+
+        int difference = (baseJoint.getTargetPosition() - baseJoint.getCurrentPosition());
+
+        return (Math.abs(difference) <= 5);
+
     }
 }
--- TeamCode/src/main/java/teamcode/XdriveSystem.java
+++ TeamCode/src/main/java/teamcode/XdriveSystem.java
@@ -5,11 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 
-@TeleOp(name = "Drive System", group = "Linear Opmode") //making the code appear in the app
+@TeleOp(name = "Game Sytem", group = "Linear Opmode") //making the code appear in the app
 
 
 
@@ -20,16 +19,22 @@ public class XdriveSystem extends LinearOpMode{ //make it work right
     private DcMotor downLeftMotor;
     private DcMotor downRightMotor;
 
+    private DcMotor liftMotor;
+
+    private DcMotor armBase;
+
+    private Servo armElbow;
+    private Servo armWrist;
+    private Servo intakeHand;
+
     private double upRightMotorPower;
-    private double upleftMotorPower;
+    private double upLeftMotorPower;
     private double downLeftMotorPower;
     private double downRightMotorPower;
 
     private double leftStickY;
     private double leftStickX;
     private double rightStickX;
-
-    private int controlMode;
 
 
 /*
@@ -40,32 +45,9 @@ put variables above here, but in the class still
     @Override
     public void runOpMode() {
 
+        initializeHardware();
+
         waitForStart();
-        runtime.reset();
-
-
-        this.upLeftMotor = hardwareMap.get(DcMotor.class, "Up Left Motor");
-        this.upRightMotor = hardwareMap.get(DcMotor.class, "Up Right Motor");
-        this.downLeftMotor = hardwareMap.get(DcMotor.class, "Down Left Motor");
-        this.downRightMotor = hardwareMap.get(DcMotor.class, "Down Right Motor");
-
-        this.downLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        this.downRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        this.upRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        this.upLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        telemetry.addData("Up Left Motor Power", this.upLeftMotor.getPower());
-        telemetry.addData("Up Right Motor Power", this.upRightMotor.getPower());
-        telemetry.addData("Down Left Motor Power", this.downLeftMotor.getPower());
-        telemetry.addData("Down Right Motor Power", this.downRightMotor.getPower());
-
-        /*
-        setup stuff goes here
-         */
-
-        /*
-        GAMEPAD BUTTON USES BELOW HERE
-         */
 
         while (opModeIsActive()) {
 
@@ -73,81 +55,70 @@ put variables above here, but in the class still
             leftStickX = -gamepad1.left_stick_x;
             rightStickX = -gamepad1.right_stick_x;
 
-
-            //this block of code works as the axis lock, simply by locking the movement to an axis if the right trigger is held
-
-
-
-            /*
-            COMMAND SETS BELOW HERE
-             */
-
-            if(gamepad1.right_trigger != 0){
-
-                sprintMovement();
-
-            }
-            else if(gamepad1.right_trigger == 0){
-
-                regularMovement();
-
-            }
-            setMotorSpeeds();
-            telemetry.update();
+            commandSet();
+            updateTelemetry();
 
         }
     }
 
+    private void commandSet() {
 
-    /*
-    METHODS FOR STUFF BELOW HERE
-     */
+        movement();
 
-    public void regularMovement() {
+        armLoop();
+
+        updateTelemetry();
+
+    }
+
+    private void regularMovement() {
 
         /*
         this method moves the bot at half speed, the fastest we want to go
         it also stops the robot, if the sticks are not moved
          */
 
-            if (rightStickX == 0 & (leftStickX != 0 || leftStickY != 0)) { //if only left stick, move
+        if (rightStickX == 0 & (leftStickX != 0 || leftStickY != 0)) { //if only left stick, move
 
-                upleftMotorPower    = ((-leftStickY / 2) - (leftStickX / 2));
-                downLeftMotorPower  = ((-leftStickY / 2) + (leftStickX / 2));
-                upRightMotorPower   = ((leftStickY / 2)  - (leftStickX / 2));
-                downRightMotorPower = ((leftStickY / 2)  + (leftStickX / 2));
+            upLeftMotorPower =      ((-leftStickY / 2) - (leftStickX / 2));
+            downLeftMotorPower =    ((-leftStickY / 2) + (leftStickX / 2));
+            upRightMotorPower =     ((leftStickY / 2)  - (leftStickX / 2));
+            downRightMotorPower =   ((leftStickY / 2)  + (leftStickX / 2));
 
-            } else if (rightStickX != 0 & (leftStickX == 0 & leftStickY == 0)) { //if only right stick, turn
+        } else if (rightStickX != 0 & (leftStickX == 0 & leftStickY == 0)) { //if only right stick, turn
 
-                upRightMotorPower   = (-rightStickX);
-                upleftMotorPower    = (-rightStickX);
-                downLeftMotorPower  = (-rightStickX);
-                downRightMotorPower = (-rightStickX);
+            upRightMotorPower =     (-rightStickX);
+            upLeftMotorPower =      (-rightStickX);
+            downLeftMotorPower =    (-rightStickX);
+            downRightMotorPower =   (-rightStickX);
 
-            } else if (rightStickX != 0 & (leftStickY != 0 || leftStickX != 0)) { //if both sticks, move and turn
+        } else if (rightStickX != 0 & (leftStickY != 0 || leftStickX != 0)) { //if both sticks, move and turn
 
-                upleftMotorPower    = (((-leftStickY / 3) - (leftStickX / 3)) - rightStickX / 3);
-                downLeftMotorPower  = (((-leftStickY / 3) + (leftStickX / 3)) - rightStickX / 3);
-                upRightMotorPower   = (((leftStickY / 3)  - (leftStickX / 3)) - rightStickX / 3);
-                downRightMotorPower = (((leftStickY / 3)  + (leftStickX / 3)) - rightStickX / 3);
+            upLeftMotorPower =      (((-leftStickY / 3) - (leftStickX / 3)) - rightStickX / 3);
+            downLeftMotorPower =    (((-leftStickY / 3) + (leftStickX / 3)) - rightStickX / 3);
+            upRightMotorPower =     (((leftStickY / 3)  - (leftStickX / 3)) - rightStickX / 3);
+            downRightMotorPower =   (((leftStickY / 3)  + (leftStickX / 3)) - rightStickX / 3);
 
-            } else { //if no sticks, stop
+        } else { //if no sticks, stop
 
-                upRightMotorPower   = 0;
-                upleftMotorPower    = 0;
-                downLeftMotorPower  = 0;
-                downRightMotorPower = 0;
+            upRightMotorPower =     0;
+            upLeftMotorPower =      0;
+            downLeftMotorPower =    0;
+            downRightMotorPower =   0;
 
-
-            }
         }
 
-    public void sprintMovement(){
+        setDriveMotorSpeeds();
+        updateTelemetry();
+
+    }
+
+    private void sprintMovement(){
 
         if(Math.abs(leftStickX) > Math.abs(leftStickY)){ //if x is greater than y, only move on the x axis
 
             upRightMotorPower   = -leftStickX;
-            upleftMotorPower    = -leftStickX;
+            upLeftMotorPower    = -leftStickX;
             downLeftMotorPower  = leftStickX;
             downRightMotorPower = leftStickX;
 
@@ -156,20 +127,20 @@ put variables above here, but in the class still
         else if(Math.abs(leftStickX) < Math.abs(leftStickY)){ //if y is greater than x, only move on the y axis
 
             upRightMotorPower   = leftStickY;
-            upleftMotorPower    = -leftStickY;
+            upLeftMotorPower    = -leftStickY;
             downLeftMotorPower  = -leftStickY;
             downRightMotorPower = leftStickY;
 
         } else if (rightStickX != 0 & (leftStickX == 0 & leftStickY == 0)) { //if only right stick, turn
 
             upRightMotorPower   = (-rightStickX);
-            upleftMotorPower    = (-rightStickX);
+            upLeftMotorPower    = (-rightStickX);
             downLeftMotorPower  = (-rightStickX);
             downRightMotorPower = (-rightStickX);
 
         } else if (rightStickX != 0 & (leftStickY != 0 || leftStickX != 0)) { //if both sticks, move and turn
 
-            upleftMotorPower    = (((-leftStickY / 3) - (leftStickX / 3)) - rightStickX / 3);
+            upLeftMotorPower    = (((-leftStickY / 3) - (leftStickX / 3)) - rightStickX / 3);
             downLeftMotorPower  = (((-leftStickY / 3) + (leftStickX / 3)) - rightStickX / 3);
             upRightMotorPower   = (((leftStickY / 3)  - (leftStickX / 3)) - rightStickX / 3);
             downRightMotorPower = (((leftStickY / 3)  + (leftStickX / 3)) - rightStickX / 3);
@@ -177,23 +148,119 @@ put variables above here, but in the class still
         }else { //if no sticks, stop
 
             upRightMotorPower   = 0;
-            upleftMotorPower    = 0;
+            upLeftMotorPower    = 0;
             downLeftMotorPower  = 0;
             downRightMotorPower = 0;
 
         }
+
+        setDriveMotorSpeeds();
+        updateTelemetry();
+
     }
 
-    public void setMotorSpeeds(){
+    private void setDriveMotorSpeeds(){
 
         //this method just sets the motor powers to the variables i told them to be at
 
         this.upRightMotor.setPower(upRightMotorPower);
-        this.upLeftMotor.setPower(upleftMotorPower);
+        this.upLeftMotor.setPower(upLeftMotorPower);
         this.downLeftMotor.setPower(downLeftMotorPower);
         this.downRightMotor.setPower(downRightMotorPower);
+
+        updateTelemetry();
+
+    }
+
+    private void armLoop(){
+
+        if(gamepad1.b){
+
+            armWrist.setPosition(0.1);
+            armElbow.setPosition(.5694);
+
+            armBase.setTargetPosition(-475);
+
+        }
+
+        if(!motorWithinTarget()){
+
+            armBase.setPower(1.0);
+
+        }
+
+        if(motorWithinTarget()){
+
+            armBase.setPower(0.0);
+
+        }
+    }
+
+    private boolean motorWithinTarget(){
+
+        int difference = (armBase.getTargetPosition() - armBase.getCurrentPosition());
+
+        return (Math.abs(difference) <= 5);//if the motor is within 5 ticks in either direction
+
+    }
+
+    private void movement(){
+
+        if(gamepad1.right_trigger != 0){
+
+            sprintMovement();
+
+        }
+        else if(gamepad1.right_trigger == 0){
+
+            regularMovement();
+
+        }
+
+    }
+
+    private void initializeHardware(){
+
+        this.upLeftMotor = hardwareMap.get(DcMotor.class, "Up Left Motor");
+        this.upRightMotor = hardwareMap.get(DcMotor.class, "Up Right Motor");
+        this.downLeftMotor = hardwareMap.get(DcMotor.class, "Down Left Motor");
+        this.downRightMotor = hardwareMap.get(DcMotor.class, "Down Right Motor");
+
+        this.liftMotor = hardwareMap.get(DcMotor.class, "Lift Motor");
+
+        this.armBase = hardwareMap.get(DcMotor.class, "Base");
+
+        this.armElbow = hardwareMap.get(Servo.class, "Elbow");
+        this.armWrist = hardwareMap.get(Servo.class, "Wrist");
+        this.intakeHand = hardwareMap.get(Servo.class, "Hand");
+
+        this.downLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.downRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.upRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.upLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        armBase.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        updateTelemetry();
+    }
+
+    private void updateTelemetry() {
+
+        telemetry.addData("Up Left Motor Power", this.upLeftMotor.getPower());
+        telemetry.addData("Up Right Motor Power", this.upRightMotor.getPower());
+        telemetry.addData("Down Left Motor Power", this.downLeftMotor.getPower());
+        telemetry.addData("Down Right Motor Power", this.downRightMotor.getPower());
+
+        telemetry.addData("lift Motor Pos", this.liftMotor.getCurrentPosition());
+
+        telemetry.addData("Arm Base Pos", this.armBase.getCurrentPosition());
+
+        telemetry.addData("Arm Elbow Pos", this.armElbow.getPosition());
+        telemetry.addData("Arm Wrist Pos", this.armWrist.getPosition());
+        telemetry.addData("Arm Hand Pos", this.intakeHand.getPosition());
 
         telemetry.update();
 
     }
+
 }
